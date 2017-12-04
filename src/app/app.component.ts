13230365f56f06676a61298659a71d57bc00395d
Ramin.Sabet <ramin.sabet.s@gmail.com>
2017-12-04 13:54:25 -0500

    [1] Fix template URL

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: '<app-employees-menu></app-employees-menu>',
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
