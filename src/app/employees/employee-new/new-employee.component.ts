98c49b9b294005a16861b2a310f3e2a040bb6a3b
Ramin.Sabet <ramin.sabet.s@gmail.com>
2017-12-07 14:38:09 -0500

    [6] Adding a method to connect component to employeeService

--- src/app/employees/employee-new/new-employee.component.ts
+++ src/app/employees/employee-new/new-employee.component.ts
@@ -1,31 +1,50 @@
+
+
+
+
+
+
+
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroupDirective, NgForm, Validators, FormBuilder } from '@angular/forms';
 import { FormGroup } from '@angular/forms/src/model';
+import { EmployeeService } from '../shared/employee.service';
+import { IEmployee } from '../shared/employee.infc';
+
 
 @Component({
   selector: 'app-new-employee',
   templateUrl: './new-employee.component.html',
   styleUrls: ['./new-employee.component.css']
 })
+
 export class NewEmployeeComponent implements OnInit {
 
-  employeeForm : FormGroup;
+  employeeForm: FormGroup;
 
-  constructor(fb: FormBuilder) {
-   
+  constructor(fb: FormBuilder, private employeeService: EmployeeService) {
+
     this.employeeForm = fb.group({
-      'firstName' : [null, Validators.required],
-      'lastName': [null, Validators.required],
-      'email' : [null, Validators.compose([Validators.required, Validators.email])]
-    })   
-   }
-
-  ngOnInit() {
+      'name': [null, Validators.required],
+      'family': [null, Validators.required],
+      'email': [null, Validators.compose([Validators.required, Validators.email])]
+    })
   }
 
-  submitForm(value: any):void{
+  ngOnInit(): void {
+  }
+
+
+  submitForm(value: IEmployee): void {
     console.log('Reactive Form Data: ')
     console.log(value);
+    this.employeeService.postEmployee(value)
+      .subscribe();
   }
 
 }
+
+
+
+
+
